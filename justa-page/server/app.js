34487b4ca68185ca3a50fd37a748e7b5784e7631
KamIlI3 <122115129+KamIlI3@users.noreply.github.com>
2024-01-22 17:22:46 +0100

    form walidation

--- justa-page/server/app.js
+++ justa-page/server/app.js
@@ -1,56 +1,85 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-const connectToDatabase = require('./config/db');
-const omnieRouter = require('./routes/omnieRouter');
-const presentationRouter = require('./routes/presentationRouter');
-const galleryRouter = require('./routes/galleryRouter');
-const productsRouter = require('./routes/productsRouter')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const connectToDatabase = require("./config/db");
+const omnieRouter = require("./routes/omnieRouter");
+const presentationRouter = require("./routes/presentationRouter");
+const galleryRouter = require("./routes/galleryRouter");
+const productsRouter = require("./routes/productsRouter");
+const formData = require("express-form-data");
+const emailValidator = require("email-validator");
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
-app.use(bodyParser.json());
 
-// Skonfiguruj transportera nodemailer
-const transporter = nodemailer.createTransport({
-  service: 'gmail', // Zmień na swojego dostawcę usług e-mail
-  auth: {
-    user: 'kami3x3s@gmail.com',
-    pass: 'xqzs wzwy fiwh szzg'
-  },
-  tls: {
-    rejectUnauthorized: false
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(bodyParser.json());
+app.use(formData.parse());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/send", (req, res) => {
+  let returnObj = {};
+  const { name, email, subject, message } = req.body;
+  if (!emailValidator.validate(email)) {
+    if (!returnObj.errors) returnObj.errors = [];
+    returnObj.errors.push("email");
+  }
+
+  if (name === undefined || name === "") {
+    if (!returnObj.errors) returnObj.errors = [];
+    returnObj.errors.push("name");
+  }
+
+  if (subject === undefined || subject === "") {
+    if (!returnObj.errors) returnObj.errors = [];
+    returnObj.errors.push("name");
+  }
+
+  if (message === undefined || message === "") {
+    if (!returnObj.errors) returnObj.errors = [];
+    returnObj.errors.push("message");
+  }
+
+  if (returnObj.errors) {
+    res.send(JSON.stringify(returnObj));
+  } else {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "kami3x3s@gmail.com",
+        pass: "xqzs wzwy fiwh szzg",
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: "kami3x3s@gmail.com",
+      subject: `${subject}`,
+      text: `Wiadomość od: ${name}\nEmail: ${email}\n\n${message}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res
+          .status(500)
+          .send("Błąd podczas wysyłania e-maila. <br> Spróbuj ponownie.");
+      } else {
+        console.log("Email wysłany: " + info.response);
+        res.send("Wiadomość wysłana pomyślnie!");
+      }
+    });
   }
 });
 
-// Endpoint do obsługi formularza kontaktowego
-app.post('/send', (req, res) => {
-  const { name, email, message } = req.body;
-
-  const mailOptions = {
-    from: email,
-    to: 'kami3x3s@gmail.com',
-    subject: `Nowa wiadomość od ${name}`,
-    text: `Wiadomość od: ${name}\nEmail: ${email}\n\n${message}`
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send('Błąd podczas wysyłania e-maila.');
-    } else {
-      console.log('Email wysłany: ' + info.response);
-      res.send('Wiadomość wysłana pomyślnie!');
-    }
-  });
-});
-
-app.use('/api/omnie', omnieRouter);
-app.use('/api/presentation', presentationRouter);
-app.use('/api/gallery', galleryRouter);
-app.use('/api/products', productsRouter)
+app.use("/api/omnie", omnieRouter);
+app.use("/api/presentation", presentationRouter);
+app.use("/api/gallery", galleryRouter);
+app.use("/api/products", productsRouter);
 
 const PORT = process.env.PORT || 3001;
 
--- justa-page/src/components/contact/ContactForm.js
+++ justa-page/src/components/contact/ContactForm.js
@@ -1,12 +1,42 @@
 // src/components/ContactForm.js
-import React, { useState } from 'react';
-import '../../css/Contact.css';
+import React, { useState } from "react";
+import "../../css/Contact.css";
 
 function ContactForm() {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const reg = /^[a-z\d]+[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i;
+  const validate = (formData) => {
+    if (!formData.name) {
+      return " Imię jest wymagane";
+    } else if (formData.name.length < 3) {
+      return " Imię musi się składać z min. 3 znaków";
+    }
+
+    if (!formData.email) {
+      return " E-mail jest wymagany";
+    } else if (!reg.test(formData.email)) {
+      return " Niepoprawny adres e-mail";
+    }
+
+    if (!formData.subject) {
+      return " Tytuł jest wymagany";
+    } else if (
+      formData.subject.charAt(0) !== formData.subject.charAt(0).toUpperCase()
+    ) {
+      return " Temat musi zaczynać się z dużej litery";
+    }
+    return null;
+  };
+
+  const [error, setError] = useState(null);
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
   });
 
   const handleChange = (e) => {
@@ -15,60 +45,99 @@ function ContactForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await fetch('http://localhost:3001/send', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData)
-      });
-      const data = await response.text();
-      console.log(data);
-      // Tutaj możesz dodać logikę po pomyślnym wysłaniu, np. wyświetlenie powiadomienia
-    } catch (error) {
-      console.error('Błąd przy wysyłaniu formularza:', error);
-      // Tutaj możesz dodać obsługę błędów
+    const errorMsg = validate(formData);
+    if (errorMsg) {
+      setError(errorMsg);
+      return;
+    } else {
+      setIsSubmitting(true);
+      try {
+        const response = await fetch("http://localhost:3001/send", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await response.text();
+        setFormData({
+          name: "",
+          email: "",
+          subject: "",
+          message: "",
+        });
+        setSuccessMessage(data);
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 3000);
+      } catch (error) {
+        setErrorMessage("Błąd podczas wysyłania. Spróbuj ponownie");
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 3000);
+      }finally {
+        setIsSubmitting(false); // Zakończenie wysyłania, niezależnie od wyniku
+      }
     }
   };
 
   return (
-    <div className='Contact'>
-      <div className='Contact-img'> 
-      <img src='Justyna.jpg' alt='asd'/>
-
+    <div className="Contact">
+      <div className="Contact-img">
+        <img src="Justyna.jpg" alt="asd" />
       </div>
-      <div className='Contact-form'>
-        <form onSubmit={handleSubmit} className='Form'>
-        <input
-        className=''
-          type="text"
-          name="name"
-          placeholder="Twoje imię"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Twój email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <textarea
-          name="message"
-          placeholder="Twoja wiadomość"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        />
-        <button type="submit">Wyślij</button>
+      <div className="Contact-form">
+        <form onSubmit={handleSubmit} className="Form">
+          {error && (
+            <div className="error-message"><p><i className="fa-solid fa-circle-info"></i>{error}</p></div>
+          )}
+
+          <label htmlFor="name">Imię:</label>
+          <input
+            className=""
+            type="text"
+            name="name"
+            placeholder="Twoje imię"
+            value={formData.name}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="email">E-mail:</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Twój email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="subject">Temat:</label>
+          <input
+            type="text"
+            name="subject"
+            placeholder="Temat"
+            value={formData.subject}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="message">Wiadomość:</label>
+          <textarea
+            name="message"
+            placeholder="Twoja wiadomość"
+            value={formData.message}
+            onChange={handleChange}
+          />
+
+          <button type="submit" className={isSubmitting ? 'loading' : ''} disabled={isSubmitting}>Wyślij</button>
+
+          {successMessage && (
+            <p className="msg-status success">{successMessage}</p>
+          )}
+
+          {errorMessage && <p className="msg-status error">{errorMessage}</p>}
         </form>
       </div>
     </div>
-    
   );
 }
 
