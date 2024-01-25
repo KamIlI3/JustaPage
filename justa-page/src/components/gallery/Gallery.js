0a860ea42a29c1ef0717f7cd817c123d738b8483
KamIlI3 <122115129+KamIlI3@users.noreply.github.com>
2024-01-25 20:37:04 +0100

    GalleryPage vizualization finished

--- justa-page/src/components/gallery/Gallery.js
+++ justa-page/src/components/gallery/Gallery.js
@@ -9,7 +9,7 @@ function Gallery() {
       <div className='Gallery-content'>
       {data.reverse().map((item) => (
         <div key={item.id} className='Gallery-image'>
-            <p>{item.description}</p>
+            {/* <p>{item.description}</p> */}
             <img src={item.imgURL} alt={item.description}/>
         </div>
         ))}
