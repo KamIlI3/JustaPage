731da84875f70b0805a051e05a2111c906868fa3
KamIlI3 <122115129+KamIlI3@users.noreply.github.com>
2024-01-31 16:24:04 +0100

    Icon counting products added, cart summary added

--- justa-page/src/components/cart/Cart.js
+++ justa-page/src/components/cart/Cart.js
@@ -4,42 +4,57 @@ import "../../css/Cart.css";
 
 function Cart() {
   const { cart, addToCart, removeFromCart } = useContext(CartContext);
+  const totalValue = cart.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
 
   return (
     <div className="Cart">
       <div className="Cart-content">
         <h2>Twój Koszyk</h2>
+        <div className="Cart-content2">
         {!cart || cart.length === 0 ? (
-          <p>Koszyk jest pusty</p>
-        ) : (
-          <>
-            <span className="Opis">
-              <p>Produkt</p>
-              <p>Cena</p>
-              <p>Ilość</p>
-              <p>Suma</p>
-              <p></p>
-            </span>
-            {cart.map((item, index) => (
-              <div key={index} className="Cart-item">
-                <img src={item.product_imgURL} alt={item.name} />
-                <h3>{item.name}</h3>
-                <p>{item.price} zł</p>
-                <span>
-                  <button onClick={() => addToCart(item, -1)}>-</button>
-                  <p>{item.quantity}</p>
-                  <button onClick={() => addToCart(item, 1)}>+</button>
+              <p>Koszyk jest pusty</p>
+            ) : (
+              <>
+          <div className="Cart-products">
+            
+                <span className="Opis">
+                  <p>Produkt</p>
+                  <p>Cena</p>
+                  <p>Ilość</p>
+                  <p>Suma</p>
+                  <p></p>
                 </span>
+                {cart.map((item, index) => (
+                  <div key={index} className="Cart-item">
+                    <img src={item.product_imgURL} alt={item.name} />
+                    <h3>{item.name}</h3>
+                    <p>{item.price} zł</p>
+                    <span>
+                      <button onClick={() => addToCart(item, -1)}>-</button>
+                      <p>{item.quantity}</p>
+                      <button onClick={() => addToCart(item, 1)}>+</button>
+                    </span>
 
-                <p>{item.quantity * item.price} zł</p>
-                <button onClick={() => removeFromCart(item)}>
-                  <i className="fa-solid fa-trash"></i>
-                </button>
-              </div>
-            ))}
-            <div className="Cart-summary"></div>
+                    <p>{item.quantity * item.price} zł</p>
+                    <button onClick={() => removeFromCart(item)}>
+                      <i className="fa-solid fa-trash"></i>
+                    </button>
+                  </div>
+                ))}
+              
+          </div>
+          <div className="Cart-summary">
+            <h2>Podsumowanie koszyka</h2>
+            <label htmlFor="suma">SUMA</label>
+            <p name="suma">{totalValue}</p>
+            <button className="Go-to-payment">Przejdz do płatności</button>
+          </div>
           </>
-        )}
+            )}
+        </div>
       </div>
     </div>
   );
--- justa-page/src/components/common/NavMenu.js
+++ justa-page/src/components/common/NavMenu.js
@@ -1,27 +1,32 @@
-import '../../css/index.css';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import "../../css/index.css";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import {CartContext} from "../cart/CartContext";
 
 function NavMenu() {
+  const { cart } = useContext(CartContext);
+  const totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <div className="Nav">
-        <div className="Nav-title">
-          <h3>
-            Justyna Gawryś
-          </h3>
-        </div>
-        <div className='Nav-subpages'>
+      <div className="Nav-title">
+        <h3>Justyna Gawryś</h3>
+      </div>
+      <div className="Nav-subpages">
         <Link to="/">Strona Główna</Link>
         <Link to="/aboutme">O Mnie</Link>
         <Link to="/gallery">Galeria</Link>
         <Link to="/contact">Kontakt</Link>
         <Link to="/shop">Sklep</Link>
-        <Link to="/cart"><i className="fa-solid fa-cart-shopping"></i></Link>
-        </div>
+        <Link to="/cart">
+        <i id='cart-icon' className="fa-solid fa-cart-shopping"></i>
+          {totalQuantity > 0 && (
+            <span className="Product-count">{totalQuantity}</span>
+          )}
+        </Link>
+      </div>
     </div>
   );
 }
-
-
 
 export default NavMenu;
